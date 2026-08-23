f029157dd10102f996b2b64795c891b450f40094
Piotr Raj <PiotrRaj@protonmail.com>
2026-08-23 16:02:13 +0200

    Add files via upload

---
+++ print_models.py
@@ -0,0 +1,10 @@
+from printing_functions import print_models as pm, show_completed_models as shm
+
+
+unprinted_designs = ['etui do telefonu', 'robot pendant', 'dwunastoscian']
+completed_models = []
+
+pm(unprinted_designs, completed_models)
+shm(completed_models)
+
+
